e47ec3f37f050c9260c3c7105a5ed8e7b75904f5
Dan Bentley <dan@numiko.com>
2021-10-17 22:37:02 +0100

    Group font sprite tests

--- tests/test_fonts.py
+++ tests/test_fonts.py
@@ -1,193 +1,179 @@
 from chip8.fonts import Font
 
 
-def test_zero():
-    output = [
-        "1111",
-        "1001",
-        "1001",
-        "1001",
-        "1111",
-    ]
+class TestSprite:
+    def test_zero(self):
+        output = [
+            "11110000",
+            "10010000",
+            "10010000",
+            "10010000",
+            "11110000",
+        ]
 
-    assert output == Font.to_sprite(Font.ZERO)
+        assert output == Font.to_sprite(Font.ZERO)
 
+    def test_one(self):
+        output = [
+            "00100000",
+            "01100000",
+            "00100000",
+            "00100000",
+            "01110000",
+        ]
 
-def test_one():
-    output = [
-        "0010",
-        "0110",
-        "0010",
-        "0010",
-        "0111",
-    ]
+        assert output == Font.to_sprite(Font.ONE)
 
-    assert output == Font.to_sprite(Font.ONE)
+    def test_two(self):
+        output = [
+            "11110000",
+            "00010000",
+            "11110000",
+            "10000000",
+            "11110000",
+        ]
 
+        assert output == Font.to_sprite(Font.TWO)
 
-def test_two():
-    output = [
-        "1111",
-        "0001",
-        "1111",
-        "1000",
-        "1111",
-    ]
+    def test_three(self):
+        output = [
+            "11110000",
+            "00010000",
+            "11110000",
+            "00010000",
+            "11110000",
+        ]
 
-    assert output == Font.to_sprite(Font.TWO)
+        assert output == Font.to_sprite(Font.THREE)
 
+    def test_four(self):
+        output = [
+            "10010000",
+            "10010000",
+            "11110000",
+            "00010000",
+            "00010000",
+        ]
 
-def test_three():
-    output = [
-        "1111",
-        "0001",
-        "1111",
-        "0001",
-        "1111",
-    ]
+        assert output == Font.to_sprite(Font.FOUR)
 
-    assert output == Font.to_sprite(Font.THREE)
+    def test_five(self):
+        output = [
+            "11110000",
+            "10000000",
+            "11110000",
+            "00010000",
+            "11110000",
+        ]
 
+        assert output == Font.to_sprite(Font.FIVE)
 
-def test_four():
-    output = [
-        "1001",
-        "1001",
-        "1111",
-        "0001",
-        "0001",
-    ]
+    def test_six(self):
+        output = [
+            "11110000",
+            "10000000",
+            "11110000",
+            "10010000",
+            "11110000",
+        ]
 
-    assert output == Font.to_sprite(Font.FOUR)
+        assert output == Font.to_sprite(Font.SIX)
 
+    def test_seven(self):
+        output = [
+            "11110000",
+            "00010000",
+            "00100000",
+            "01000000",
+            "01000000",
+        ]
 
-def test_five():
-    output = [
-        "1111",
-        "1000",
-        "1111",
-        "0001",
-        "1111",
-    ]
+        assert output == Font.to_sprite(Font.SEVEN)
 
-    assert output == Font.to_sprite(Font.FIVE)
+    def test_eight(self):
+        output = [
+            "11110000",
+            "10010000",
+            "11110000",
+            "10010000",
+            "11110000",
+        ]
 
+        assert output == Font.to_sprite(Font.EIGHT)
 
-def test_six():
-    output = [
-        "1111",
-        "1000",
-        "1111",
-        "1001",
-        "1111",
-    ]
+    def test_nine(self):
+        output = [
+            "11110000",
+            "10010000",
+            "11110000",
+            "00010000",
+            "11110000",
+        ]
 
-    assert output == Font.to_sprite(Font.SIX)
+        assert output == Font.to_sprite(Font.NINE)
 
+    def test_a(self):
+        output = [
+            "11110000",
+            "10010000",
+            "11110000",
+            "10010000",
+            "10010000",
+        ]
 
-def test_seven():
-    output = [
-        "1111",
-        "0001",
-        "0010",
-        "0100",
-        "0100",
-    ]
+        assert output == Font.to_sprite(Font.A)
 
-    assert output == Font.to_sprite(Font.SEVEN)
+    def test_b(self):
+        output = [
+            "11100000",
+            "10010000",
+            "11100000",
+            "10010000",
+            "11100000",
+        ]
 
+        assert output == Font.to_sprite(Font.B)
 
-def test_eight():
-    output = [
-        "1111",
-        "1001",
-        "1111",
-        "1001",
-        "1111",
-    ]
+    def test_c(self):
+        output = [
+            "11110000",
+            "10000000",
+            "10000000",
+            "10000000",
+            "11110000",
+        ]
 
-    assert output == Font.to_sprite(Font.EIGHT)
+        assert output == Font.to_sprite(Font.C)
 
+    def test_d(self):
+        output = [
+            "11100000",
+            "10010000",
+            "10010000",
+            "10010000",
+            "11100000",
+        ]
 
-def test_nine():
-    output = [
-        "1111",
-        "1001",
-        "1111",
-        "0001",
-        "1111",
-    ]
+        assert output == Font.to_sprite(Font.D)
 
-    assert output == Font.to_sprite(Font.NINE)
+    def test_e(self):
+        output = [
+            "11110000",
+            "10000000",
+            "11110000",
+            "10000000",
+            "11110000",
+        ]
 
+        assert output == Font.to_sprite(Font.E)
 
-def test_a():
-    output = [
-        "1111",
-        "1001",
-        "1111",
-        "1001",
-        "1001",
-    ]
+    def test_f(self):
+        output = [
+            "11110000",
+            "10000000",
+            "11110000",
+            "10000000",
+            "10000000",
+        ]
 
-    assert output == Font.to_sprite(Font.A)
-
-
-def test_b():
-    output = [
-        "1110",
-        "1001",
-        "1110",
-        "1001",
-        "1110",
-    ]
-
-    assert output == Font.to_sprite(Font.B)
-
-
-def test_c():
-    output = [
-        "1111",
-        "1000",
-        "1000",
-        "1000",
-        "1111",
-    ]
-
-    assert output == Font.to_sprite(Font.C)
-
-
-def test_d():
-    output = [
-        "1110",
-        "1001",
-        "1001",
-        "1001",
-        "1110",
-    ]
-
-    assert output == Font.to_sprite(Font.D)
-
-
-def test_e():
-    output = [
-        "1111",
-        "1000",
-        "1111",
-        "1000",
-        "1111",
-    ]
-
-    assert output == Font.to_sprite(Font.E)
-
-
-def test_f():
-    output = [
-        "1111",
-        "1000",
-        "1111",
-        "1000",
-        "1000",
-    ]
-
-    assert output == Font.to_sprite(Font.F)
+        assert output == Font.to_sprite(Font.F)
