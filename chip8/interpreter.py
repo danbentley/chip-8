9e49c2e681c4354a95367824a9e2098f79d1cd3c
Dan Bentley <dan@numiko.com>
2021-10-17 22:56:46 +0100

    Fix issue where fonts were stored in memory as lists

--- chip8/interpreter.py
+++ chip8/interpreter.py
@@ -3,7 +3,7 @@ import pygame
 from chip8.memory import Memory
 from chip8.fonts import Font
 from chip8.display import Display
-from chip8.cpu import CPU, Registers
+from chip8.cpu import CPU, Registers, FONT_ADDRESS_START, FONT_ADDRESS_END
 
 
 class Interpreter:
@@ -14,8 +14,8 @@ class Interpreter:
 
     def boot(self):
         fonts = Font.__members__.values()
-        for font, location in zip(fonts, range(0x050, 0x0A0)):
-            self.memory[location] = font.value
+        for font, location in zip(fonts, range(FONT_ADDRESS_START, FONT_ADDRESS_END, 5)):
+            self.memory[location: location + 5] = font.value
 
     def load_rom(self, path):
         with open(path, "rb") as f:
