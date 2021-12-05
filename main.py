0f154ccc37d13c38d95560f67f8966ab878fac54
Dan Bentley <dan@numiko.com>
2021-12-05 21:49:53 +0000

    Allow switching of backends via CLI

--- main.py
+++ main.py
@@ -7,26 +7,33 @@ from chip8.backends.pysdl import PySDLBackend, Display as SDLDisplay
 from chip8.cpu import CPU, Registers
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", help="Path to rom file")
-    args = parser.parse_args()
-
+def main(rom_path, backend_name):
     memory = Memory()
 
-    # display = PyGameDisplay(width=64, height=32, scale=4)
-    # backend = PyGameBackend()
-
-    display = SDLDisplay(width=64, height=32, scale=4)
-    backend = PySDLBackend()
+    if backend_name == "pygame":
+        display = PyGameDisplay(width=64, height=32, scale=4)
+        backend = PyGameBackend()
+    else:
+        display = SDLDisplay(width=64, height=32, scale=4)
+        backend = PySDLBackend()
 
     cpu = CPU(memory, display, Registers())
 
-    interpreter = Interpreter(memory, display, backend, cpu)
+    interpreter = Interpreter(backend, cpu)
     interpreter.boot()
-    interpreter.load_rom(args.path)
+    interpreter.load_rom(rom_path)
     interpreter.run()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str, help="Path to rom file")
+    parser.add_argument(
+        "--backend",
+        help="Backend used to run the interpreter",
+        choices=["pygame", "sdl"],
+        default="pygame",
+    )
+    args = parser.parse_args()
+
+    main(args.path, args.backend)
